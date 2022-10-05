eb9530825af746fd98a0443295ef0df21f7d0686
Lehlomela <lehlomela@psybergate.co.za>
2022-10-05 20:55:51 +0200

    added base repos

--- src/main/java/com/example/demo/repository/MyBaseRepository.java
+++ src/main/java/com/example/demo/repository/MyBaseRepository.java
@@ -1,2 +1,13 @@
-package com.example.demo.repository;public class MyBaseRepository {
+package com.example.demo.repository;
+
+import com.example.demo.domain.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+/*
+ * todo::
+ */
+@NoRepositoryBean
+public interface MyBaseRepository extends JpaRepository<Customer, Long> {
+
 }
