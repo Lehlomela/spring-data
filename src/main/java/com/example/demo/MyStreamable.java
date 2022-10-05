13c344c6c0ff7864cf41c72149bf195c950804b0
Lehlomela <lehlomela@psybergate.co.za>
2022-10-05 09:00:33 +0200

    streamable DI

--- src/main/java/com/example/demo/MyStreamable.java
+++ src/main/java/com/example/demo/MyStreamable.java
@@ -1,8 +1,16 @@
 package com.example.demo;
 
+import com.example.demo.repository.CustomerRepository;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyStreamable {
+
+  private CustomerRepository customerRepository;
+  
+  public MyStreamable(final CustomerRepository customerRepository) {
+    this.customerRepository = customerRepository;
+  }
+
 
 }
